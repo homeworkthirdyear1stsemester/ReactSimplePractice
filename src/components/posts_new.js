7031bfea0a46d3ebc60bd64f31b375604bb9c634
kihwankim <clack2933@gmail.com>
2019-08-05 17:39:07 +0900

    add form tag and input tag

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderTitleField(filed) { //filed인자를 받아서 input 테스의 정보를 가져간다
+      return (
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            className = "form-control"
+            type="text"
+            {...filed.input}
+          />
+        </div>
+      );
+  }// input의 모든 event를 ...filed.input으로 가지도록 한다
+
   render() {
     return (
-      <div>
-        PostsNew
-      </div>
+      <form>
+        <Field
+          name="title"
+          component={ this.renderTitleField }
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
+//redux form 으로 지정 한다
+//form의 이름을 지정 -> PostsNewForm은 하나의 key로 지정
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer} from 'redux-form';
+//reducer 라는 property를 formReducer로 변경 하는 것
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
