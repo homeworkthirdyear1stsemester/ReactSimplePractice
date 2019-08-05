fd6d08856ac95301f0c65f420fffd2016eb43dd8
kihwankim <clack2933@gmail.com>
2019-08-05 14:46:45 +0900

    practice

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route } from "react-router-dom";
+import promise from "redux-promise";
+
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+class Hello extends React.Component {
+  render(){
+    return <div>Hello!!</div>
+  }
+}
+
+class Goodbye extends React.Component {
+  render(){
+    return <div>GoodBye!!</div>
+  }
+}
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector(".container")
+);
