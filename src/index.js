e8e005a3f9822da0bacf09a7ac5437d467a78d4c
kihwankim <clack2933@gmail.com>
2019-08-05 15:33:30 +0900

    lessson2

--- src/index.js
+++ src/index.js
@@ -6,29 +6,17 @@ import { BrowserRouter, Route } from "react-router-dom";
 import promise from "redux-promise";
 
 import reducers from "./reducers";
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
-
-class Hello extends React.Component {
-  render(){
-    return <div>Hello!!</div>
-  }
-}
-
-class Goodbye extends React.Component {
-  render(){
-    return <div>GoodBye!!</div>
-  }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
-  </Provider>,
-  document.querySelector(".container")
+  </Provider>
+  , document.querySelector(".container")
 );
