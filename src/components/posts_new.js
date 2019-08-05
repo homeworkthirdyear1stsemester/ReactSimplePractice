bb63a87153bc1d8668d9bedb1a3a717a9692d7e1
kihwankim <clack2933@gmail.com>
2019-08-05 18:47:01 +0900

    add submit function for temp

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,13 +11,20 @@ class PostsNew extends Component {
             type="text"
             {...field.input}
           />
+          { field.meta.error }
         </div>
       );
   }// input의 모든 event를 ...filed.input으로 가지도록 한다
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
         <Field
           label="Title For Post"
           name="title"
@@ -33,9 +40,12 @@ class PostsNew extends Component {
           name="content"
           component={ this.renderField }
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
+  //render의 name이랑 validate가 reduxForm에 의해 연결 되어있으므로 meta로 validate의
+  //리터 객체의 같은 name의 값과 접근 가능
 }
 
 function validate(values) {
@@ -46,10 +56,10 @@ function validate(values) {
     errors.title = "Enter a title!";
   }
   if (!values.categories) {
-    errors.categories = "Enter some categories";
+    errors.categories = "Enter some categories!";
   }
   if (!values.content) {
-    errors.content = "Enter some content please";
+    errors.content = "Enter some content please!";
   }
 
   return errors;
